7b9aca9cb824f7a0d4855f36102ee35a66e28b5d
masfazan <msfazan@icloud.com>
2024-04-18 20:56:27 -0300

    Add project files.

---
+++ OperacoesBasicas/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite um número");
+float n1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite um número");
+float n2 = float.Parse(Console.ReadLine());
+
+float soma = n1 + n2;
+float sub = n1 - n2;
+float mult = n1 * n2;
+float div = n2 / n2;
+
+Console.WriteLine("A soma é igual a " + soma + "\nA subtração é igual a " + sub + "\nA divisão é igual a " + div + "\nA multiplicação é igual a " + mult);
+Console.ReadLine();
