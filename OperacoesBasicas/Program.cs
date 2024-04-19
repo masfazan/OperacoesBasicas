97c230b05805e6c395668b60aef880b7a75b20e8
masfazan <msfazan@icloud.com>
2024-04-19 10:19:43 -0300

    QuatroOperacoes

--- OperacoesBasicas/Program.cs
+++ OperacoesBasicas/Program.cs
@@ -1,37 +1,49 @@
-﻿Console.WriteLine("Digite o primeiro número");
-float n1 = float.Parse(Console.ReadLine());
-Console.WriteLine("Digite o segundo número");
-float n2 = float.Parse(Console.ReadLine());
+﻿float n1=0, n2=0, n3=0, n4=0, resultado;
+int operacao;
+
+Console.WriteLine("Digite o primeiro número");
+n1 = float.Parse(Console.ReadLine());
+
+while (n2 == 0)
+{
+    Console.WriteLine("Digite o segundo número");
+    n2 = float.Parse(Console.ReadLine());
+}
+
+do
+{
+    Console.WriteLine("Digite o terceiro número");
+    n3 = float.Parse(Console.ReadLine());
+} while (n3 == 0);
+
+for (n4 = 0; n4 == 0;)
+{
+    Console.WriteLine("Digite o quarto número");
+    n4 = float.Parse(Console.ReadLine());
+}
 
 Console.WriteLine("Escolha a operação desejada: \n1-Soma\n2-Subtracao\n3-Multiplicacao\n4-Divisao");
-int operacao = int.Parse(Console.ReadLine());
+operacao = int.Parse(Console.ReadLine());
 
 if (operacao == 1)
 {
-    float soma = n1 + n2;
-    Console.WriteLine("A soma é igual a " + soma);
+    resultado = n1 + n2 + n3 + n4;
+    Console.WriteLine("A soma é igual a " + resultado);
 }
 else if (operacao == 2)
 { 
-    float sub = n1 - n2;
-    Console.WriteLine("A subtracao é igual a " + sub);
+    resultado = n1 - n2 - n3 - n4;
+    Console.WriteLine("A subtracao é igual a " + resultado);
 }
 else if (operacao == 3)
 {
-    float mult = n1 * n2;
-    Console.WriteLine("A multiplicacao é igual a " + mult);
+    resultado = n1 * n2 * n3 * n4;
+    Console.WriteLine("A multiplicacao é igual a " + resultado);
 }
-else if (operacao == 4)
+else 
 {
-    if (n2 == 0)
-    {
-        Console.WriteLine("Não existe divisão por zero!");
-    }
-    else
-    {
-        float div = n1 / n2;
-        Console.WriteLine("A divisao é igual a " + div);
-    }
+    resultado = n1 / n2 / n3 / n4;
+    Console.WriteLine("A divisao é igual a " + resultado);
 }
 
 Console.ReadLine();
